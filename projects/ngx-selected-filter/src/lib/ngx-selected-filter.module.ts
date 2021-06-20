d3eddf8a73a7c8a3d1a53d88130ace3c8450474e
hemanthalwai <37038391+hemanthalwai@users.noreply.github.com>
2021-06-20 20:46:32 +0530

    Add component on module exports (to avoid getting build errors)

--- projects/ngx-selected-filter/src/lib/ngx-selected-filter.module.ts
+++ projects/ngx-selected-filter/src/lib/ngx-selected-filter.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
+import { NgxSelectedFilterComponent } from './ngx-selected-filter.component';
 // import { NgxSelectedFilterComponent } from './ngx-selected-filter.component';
 import { SelectedFilterScopeDirective } from './selected-filter-scope.directive';
 import { SelectedFilterDirective } from './selected-filter.directive';
 
 @NgModule({
-  declarations: [SelectedFilterScopeDirective, SelectedFilterDirective],
+  declarations: [SelectedFilterScopeDirective, SelectedFilterDirective, NgxSelectedFilterComponent],
   imports: [
   ],
-  exports: [SelectedFilterScopeDirective, SelectedFilterDirective]
+  exports: [SelectedFilterScopeDirective, SelectedFilterDirective, NgxSelectedFilterComponent]
 })
 export class NgxSelectedFilterModule { }
